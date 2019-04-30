52fc1455ab7f93a2b1dd4662323572a4d1cbcb39
AlphaNiner1415 <manchesternd@gmail.com>
2019-04-30 15:08:01 +0700

    draw method on it's way

--- Triangle.java
+++ Triangle.java
@@ -7,7 +7,12 @@ public class Triangle extends Shape{
         super(rows, character);
     }
     public void draw(){
-        
+        for(int i = 0; i < this.rows; i++){
+            System.out.println(character);
+        }
+        for (int j = 0; j< super.rows; j++){
+            System.out.println(character);
+        }
     }
 
 }
