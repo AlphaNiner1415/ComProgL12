82f4a0869495b243f7d04907f752932b1a639ec1
AlphaNiner1415 <manchesternd@gmail.com>
2019-04-30 14:35:13 +0700

    Update Shape.java
    2nd commit

---
+++ Shape.java
@@ -0,0 +1,24 @@
+public class Shape{
+    protected int rows;
+    protected char character;
+
+    public Shape(){
+        this.rows = 0;
+        this.character = '';
+    }
+    public Shape(int x, char c){
+
+    }
+    public int getRows(){
+        return this.rows;
+    }
+    public char getCharacter(){
+        return this.character;
+    } 
+    public void setRows(int x){
+        this.rows = x;
+    }
+    public void setCharacter(char c){
+        this.character = c;
+    }
+}
