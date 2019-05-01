015a8ae6bcd18ce140d03dc128b2912576a68c28
Copter <copter12349@hotmail.com>
2019-05-01 10:21:43 +0700

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,10 +1,10 @@
 public class Square extends Shape{
   public Square(){
-    this.rows = 0;
-    this.character = '';
+    super(rows, ' ');
   }
 
   public Square(int in_rows, char in_character){
+    super(rows, ' ');
   }
 
   public draw(){
@@ -15,7 +15,15 @@ public class Square extends Shape{
     return this.rows;
   }
 
-  public draw(int in_x, int in_y){
-    return this.rows;
+  public int getArea(){
+
+  }
+
+  public int getPerimeter(){
+
+  }
+
+  public String toString() {
+    return "Rows: " + rows + ", Character: " + character;
   }
 }
