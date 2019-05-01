339bb9d6440c57c38c803a8b0c3801a9a9cd4e49
Copter <copter12349@hotmail.com>
2019-05-01 10:39:22 +0700

    I got the square, boi

---
+++ MyTest.java
@@ -0,0 +1,6 @@
+public class MyTest{
+    public static void main(String[] args) {
+        Square sq1 = new Square(5,'X');
+        sq1.draw(0, 0);
+    }
+}
--- Square.java
+++ Square.java
@@ -1,26 +1,42 @@
 public class Square extends Shape{
-  public Square(){
-    super(rows, ' ');
+  public Square(int rows){
+    super(rows, '*');
   }
 
-  public Square(int in_rows, char in_character){
-    super(rows, ' ');
+  public Square(int rows, char character){
+    super(rows, character);
   }
 
-  public draw(){
-    return this.rows;
+  public void draw(){
+    for(int i=0; i<this.rows; i++){
+      for(int j=0; j<this.rows; j++){
+        System.out.print(this.character);
+      }
+      System.out.print("\n");
+    }
   }
 
-  public draw(int in_x, int in_y){
-    return this.rows;
+  public void draw(int in_x, int in_y){
+    for(int y0=0; y0<in_y; y0++){
+      System.out.print("\n");
+    }
+    for(int i=0; i<this.rows; i++){
+      for(int x0=0; x0<in_x; x0++){
+        System.out.print(" ");
+      }
+      for(int j=0; j<this.rows; j++){
+        System.out.print(this.character);
+      }
+      System.out.print("\n");
+    }
   }
 
   public int getArea(){
-
+      return this.rows*this.rows;
   }
 
   public int getPerimeter(){
-
+      return 4*this.rows;
   }
 
   public String toString() {
